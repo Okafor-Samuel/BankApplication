ad06408df0ded790c2b0ab1a66c8ea57c52bef64
decagon <samuel.okafor@decagon.dev>
2023-09-06 23:09:46 +0100

    Bank Application

---
+++ src/main/java/com/example/bankapplication/Entity/User.java
@@ -0,0 +1,9 @@
+package com.example.bankapplication.Entity;
+
+public class User {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String otherName;
+
+}
---
+++ src/test/java/com/example/bankapplication/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
