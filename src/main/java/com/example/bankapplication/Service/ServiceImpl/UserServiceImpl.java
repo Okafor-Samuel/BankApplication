dcd5b490cf2b4f0de58865a56fd88a0495791fb0
decagon <samuel.okafor@decagon.dev>
2023-09-07 00:30:17 +0100

    UPDATE

---
+++ src/main/java/com/example/bankapplication/Service/ServiceImpl/UserServiceImpl.java
@@ -0,0 +1,49 @@
+package com.example.bankapplication.Service.ServiceImpl;
+
+import com.example.bankapplication.Dto.BankResponseDto;
+import com.example.bankapplication.Dto.UserDto;
+import com.example.bankapplication.Entity.User;
+import com.example.bankapplication.Repository.UserRepository;
+import com.example.bankapplication.Service.UserService;
+import com.example.bankapplication.Utils.AccountUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    @Override
+    public BankResponseDto createAccount(UserDto userDto) {
+        if(userRepository.existsByEmail(userDto.getEmail())){
+            var response = BankResponseDto.builder()
+                    .responseCode(AccountUtils.ACCOUNT_EXISTS_CODE)
+                    .responseMessage(AccountUtils.ACCOUNT_EXISTS_MESSAGE)
+                    .accountInfo(null)
+                    .build();
+            return response;
+        }
+
+        var newUser = User.builder()
+                .firstName(userDto.getFirstName())
+                .lastName(userDto.getLastName())
+                .otherName(userDto.getOtherName())
+                .gender(userDto.getGender())
+                .address(userDto.getAddress())
+                .stateOfOrigin(userDto.getStateOfOrigin())
+                .accountNumber(AccountUtils.generateAccountNumber())
+                .accountBalance(BigDecimal.ZERO)
+                .email(userDto.getEmail())
+                .phoneNumber(userDto.getPhoneNumber())
+                .alternativePhoneNumber(userDto.getAlternativePhoneNumber())
+                .status("ACTIVE")
+                .build();
+        var savedUser = userRepository.save(newUser);
+        return BankResponseDto.builder()
+                .responseCode(AccountUtils.ACCOUNT_CREATION_CODE)
+                .responseMessage(AccountUtils.ACCOUNT_CREATION_MESSAGE)
+                .build();
+    }
+}
