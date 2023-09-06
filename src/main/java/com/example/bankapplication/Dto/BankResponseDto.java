5fc7f276ef26dd6744fec5676aeb07e57f3145b5
decagon <samuel.okafor@decagon.dev>
2023-09-07 00:29:17 +0100

    UPDATE

--- src/main/java/com/example/bankapplication/Dto/BankResponseDto.java
+++ src/main/java/com/example/bankapplication/Dto/BankResponseDto.java
@@ -1,4 +1,16 @@
 package com.example.bankapplication.Dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class BankResponseDto {
+    private String responseCode;
+    private String responseMessage;
+    private AccountInfo accountInfo;
 }
