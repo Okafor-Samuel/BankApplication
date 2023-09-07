f8acba1365f566283b3c9b75923832e6a2bb777b
decagon <samuel.okafor@decagon.dev>
2023-09-07 14:52:15 +0100

    Enquiry UPDATE

--- src/main/java/com/example/bankapplication/Repository/UserRepository.java
+++ src/main/java/com/example/bankapplication/Repository/UserRepository.java
@@ -4,7 +4,11 @@ import com.example.bankapplication.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByEmail(String email);
+    Boolean existsByAccountNumber(String accountNumber);
+    Optional<User> findByAccountNumber(String accountNumber);
 }
