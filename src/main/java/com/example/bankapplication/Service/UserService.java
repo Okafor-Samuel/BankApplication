ad2558d060fa1a60df1507f89bb03935b82c1eba
decagon <samuel.okafor@decagon.dev>
2023-09-07 14:52:31 +0100

    Enquiry UPDATE

--- src/main/java/com/example/bankapplication/Service/UserService.java
+++ src/main/java/com/example/bankapplication/Service/UserService.java
@@ -1,8 +1,11 @@
 package com.example.bankapplication.Service;
 
 import com.example.bankapplication.Dto.BankResponseDto;
+import com.example.bankapplication.Dto.EnquiryDto;
 import com.example.bankapplication.Dto.UserDto;
 
 public interface UserService {
     BankResponseDto createAccount(UserDto userDto);
+    BankResponseDto balanceEnquiry(EnquiryDto enquiryDto);
+    String nameEnquiry(EnquiryDto enquiryDto);
 }
