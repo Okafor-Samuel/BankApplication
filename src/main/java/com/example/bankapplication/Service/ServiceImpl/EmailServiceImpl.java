93d75ba463fbc3654b2f9391bd618659fb4d0fd2
decagon <samuel.okafor@decagon.dev>
2023-09-07 11:04:45 +0100

    Email UPDATE

---
+++ src/main/java/com/example/bankapplication/Service/ServiceImpl/EmailServiceImpl.java
@@ -0,0 +1,35 @@
+package com.example.bankapplication.Service.ServiceImpl;
+
+import com.example.bankapplication.Dto.EmailDto;
+import com.example.bankapplication.Service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailServiceImpl implements EmailService {
+
+    private final JavaMailSender javaMailSender;
+    @Value("${spring.mail.username}")
+    private String senderEmail;
+
+    @Override
+    public void sendEmailAlert(EmailDto emailDto) {
+        try {
+            var mailMessage = new SimpleMailMessage();
+            mailMessage.setFrom(senderEmail);
+            mailMessage.setTo(emailDto.getRecipient());
+            mailMessage.setText(emailDto.getMessageBody());
+            mailMessage.setSubject(emailDto.getSubject());
+            javaMailSender.send(mailMessage);
+            System.out.println("Mail sent successfully");
+        } catch (MailException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
