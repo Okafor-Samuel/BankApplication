b7655a5be42086fcc0a37bd2dd799bc90a370b45
decagon <samuel.okafor@decagon.dev>
2023-09-07 01:03:36 +0100

    UPDATE

--- src/main/java/com/example/bankapplication/Dto/UserDto.java
+++ src/main/java/com/example/bankapplication/Dto/UserDto.java
@@ -1,6 +1,8 @@
 package com.example.bankapplication.Dto;
 
 import com.example.bankapplication.Enum.Gender;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,6 +17,7 @@ public class UserDto {
     private String firstName;
     private String lastName;
     private String otherName;
+    @Enumerated(value = EnumType.STRING)
     private Gender gender;
     private String address;
     private String stateOfOrigin;
