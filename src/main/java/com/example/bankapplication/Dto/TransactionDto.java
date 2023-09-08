fd5241c9353c40bd06a56a52519c6ad596081e15
decagon <samuel.okafor@decagon.dev>
2023-09-08 04:16:17 +0100

    Transaction UPDATE

---
+++ src/main/java/com/example/bankapplication/Dto/TransactionDto.java
@@ -0,0 +1,17 @@
+package com.example.bankapplication.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TransactionDto {
+    private String accountNumber;
+    private BigDecimal amount;
+}
