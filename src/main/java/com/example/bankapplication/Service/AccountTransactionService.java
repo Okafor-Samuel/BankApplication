e5ebe2bd0b694e8fecec9f6a0271664900d8436f
decagon <samuel.okafor@decagon.dev>
2023-09-08 20:25:29 +0100

    accountTransaction UPDATE

--- src/main/java/com/example/bankapplication/Service/AccountTransactionService.java
+++ src/main/java/com/example/bankapplication/Service/AccountTransactionService.java
@@ -1,7 +1,8 @@
 package com.example.bankapplication.Service;
 
+import com.example.bankapplication.Dto.AccountTransactionDto;
 import com.example.bankapplication.Entity.AccountTransaction;
 
 public interface AccountTransactionService {
-    void saveTransaction(AccountTransaction accountTransaction);
+    void saveTransaction(AccountTransactionDto accountTransactionDto);
 }
