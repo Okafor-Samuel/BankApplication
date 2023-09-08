4826bb59496cc6c943b336b7660081d6b5b6654a
decagon <samuel.okafor@decagon.dev>
2023-09-08 20:25:17 +0100

    accountTransaction UPDATE

--- src/main/java/com/example/bankapplication/Repository/AccountTransactionRepository.java
+++ src/main/java/com/example/bankapplication/Repository/AccountTransactionRepository.java
@@ -2,6 +2,8 @@ package com.example.bankapplication.Repository;
 
 import com.example.bankapplication.Entity.AccountTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccountTransactionRepository extends JpaRepository<AccountTransaction, String> {
 }
