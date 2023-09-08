532ec6d6380c1460e9d3755ff0b269c14a5ffdaa
decagon <samuel.okafor@decagon.dev>
2023-09-08 04:16:36 +0100

    Transaction UPDATE

--- src/main/java/com/example/bankapplication/Service/UserService.java
+++ src/main/java/com/example/bankapplication/Service/UserService.java
@@ -2,10 +2,12 @@ package com.example.bankapplication.Service;
 
 import com.example.bankapplication.Dto.BankResponseDto;
 import com.example.bankapplication.Dto.EnquiryDto;
+import com.example.bankapplication.Dto.TransactionDto;
 import com.example.bankapplication.Dto.UserDto;
 
 public interface UserService {
     BankResponseDto createAccount(UserDto userDto);
     BankResponseDto balanceEnquiry(EnquiryDto enquiryDto);
     String nameEnquiry(EnquiryDto enquiryDto);
+    BankResponseDto creditAccount(TransactionDto transactionDto);
 }
