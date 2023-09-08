d807a3e6d707da498ad38302f8bfd928dd216c7e
decagon <samuel.okafor@decagon.dev>
2023-09-08 20:25:38 +0100

    accountTransaction UPDATE

--- src/main/java/com/example/bankapplication/Service/ServiceImpl/AccountTransactionServiceImpl.java
+++ src/main/java/com/example/bankapplication/Service/ServiceImpl/AccountTransactionServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.bankapplication.Service.ServiceImpl;
 
+import com.example.bankapplication.Dto.AccountTransactionDto;
 import com.example.bankapplication.Entity.AccountTransaction;
+import com.example.bankapplication.Repository.AccountTransactionRepository;
 import com.example.bankapplication.Service.AccountTransactionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,8 +10,19 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AccountTransactionServiceImpl implements AccountTransactionService {
+    private final AccountTransactionRepository transactionRepository;
     @Override
-    public void saveTransaction(AccountTransaction accountTransaction) {
-
+    public void saveTransaction(AccountTransactionDto accountTransactionDto) {
+    var accountTransaction = AccountTransaction.builder()
+            .transactionType(accountTransactionDto.getTransactionType())
+            .accountNumber(accountTransactionDto.getAccountNumber())
+            .transactionAmount(accountTransactionDto.getTransactionAmount())
+            .status("SUCCESS")
+            .build();
+    transactionRepository.save(accountTransaction);
+        System.out.println("Transaction saved successfully");
     }
+
+
+
 }
