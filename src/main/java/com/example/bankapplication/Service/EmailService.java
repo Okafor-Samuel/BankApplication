64d16d24c0289feea1ecd386936cf7ecd55b5333
decagon <samuel.okafor@decagon.dev>
2023-09-09 13:16:03 +0100

    accountTransaction UPDATE

--- src/main/java/com/example/bankapplication/Service/EmailService.java
+++ src/main/java/com/example/bankapplication/Service/EmailService.java
@@ -4,4 +4,5 @@ import com.example.bankapplication.Dto.EmailDto;
 
 public interface EmailService {
     void sendEmailAlert(EmailDto emailDto);
+    void sendEmailWithAttachment(EmailDto emailDto);
 }
