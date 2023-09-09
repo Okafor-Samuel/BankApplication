ce9117ae27e5066fafc8d3ac057b56a5e39defb2
decagon <samuel.okafor@decagon.dev>
2023-09-10 00:16:23 +0100

    accountTransaction UPDATE

--- src/main/java/com/example/bankapplication/Service/ServiceImpl/EmailServiceImpl.java
+++ src/main/java/com/example/bankapplication/Service/ServiceImpl/EmailServiceImpl.java
@@ -2,16 +2,24 @@ package com.example.bankapplication.Service.ServiceImpl;
 
 import com.example.bankapplication.Dto.EmailDto;
 import com.example.bankapplication.Service.EmailService;
+import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmailServiceImpl implements EmailService {
 
     private final JavaMailSender javaMailSender;
@@ -36,6 +44,23 @@ public class EmailServiceImpl implements EmailService {
 
     @Override
     public void sendEmailWithAttachment(EmailDto emailDto) {
+    MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper;
+        try {
+          mimeMessageHelper =  new MimeMessageHelper(mimeMessage,true);
+          mimeMessageHelper.setFrom(senderEmail);
+          mimeMessageHelper.setTo(emailDto.getRecipient());
+          mimeMessageHelper.setText(emailDto.getMessageBody());
+          mimeMessageHelper.setSubject(emailDto.getSubject());
+
+          var file = new FileSystemResource(new File(emailDto.getAttachment()));
+          mimeMessageHelper.addAttachment(Objects.requireNonNull(file.getFilename()), file);
+          javaMailSender.send(mimeMessage);
+
+            log.info(file.getFilename()+ " has ben sent to to user with email "+emailDto.getRecipient());
+        }catch (MessagingException e){
+            throw new RuntimeException(e);
+        }
 
     }
 }
