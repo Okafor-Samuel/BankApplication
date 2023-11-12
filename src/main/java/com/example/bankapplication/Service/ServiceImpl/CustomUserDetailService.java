2817d5650b73babb0ad30c58d7abeed9863bd70a
Samuel <samuel.okafor@decagon.dev>
2023-11-12 15:57:41 +0100

    security

---
+++ src/main/java/com/example/bankapplication/Service/ServiceImpl/CustomUserDetailService.java
@@ -0,0 +1,12 @@
+package com.example.bankapplication.Service.ServiceImpl;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
