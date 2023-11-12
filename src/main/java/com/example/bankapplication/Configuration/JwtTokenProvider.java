91e4f039c5d8bb5d7690f965fef4de42ca3d6c2a
Samuel <samuel.okafor@decagon.dev>
2023-11-12 15:58:11 +0100

    security

---
+++ src/main/java/com/example/bankapplication/Configuration/JwtTokenProvider.java
@@ -0,0 +1,49 @@
+package com.example.bankapplication.Configuration;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${app.jwt-secrete}")
+    private String jwtSecrete;
+    @Value("${app.jwt-expiration}")
+    private Long jwtExpirationDate;
+
+public String generateToken(Authentication authentication){
+    String userName =authentication.getName();
+    Date currentDate = new Date();
+    Date expireDate = new Date(currentDate.getTime() + jwtExpirationDate);
+
+    return Jwts.builder()
+            .setSubject(userName)
+            .setIssuedAt(currentDate)
+            .setExpiration(expireDate)
+            .signWith(key())
+            .compact();
+    }
+
+    private Key key(){
+        byte[] bytes = Decoders.BASE64.decode(jwtSecrete);
+        return Keys.hmacShaKeyFor(bytes);
+    }
+
+    public String getUserName(String token){
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+}
