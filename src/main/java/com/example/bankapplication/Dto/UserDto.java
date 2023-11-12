e9ae44fced4f84bb3105c27d1bbb9d50631d9acb
Samuel <samuel.okafor@decagon.dev>
2023-11-12 15:24:29 +0100

    security

--- src/main/java/com/example/bankapplication/Dto/UserDto.java
+++ src/main/java/com/example/bankapplication/Dto/UserDto.java
@@ -22,6 +22,7 @@ public class UserDto {
     private String address;
     private String stateOfOrigin;
     private String email;
+    private String password;
     private String phoneNumber;
     private String alternativePhoneNumber;
 }
