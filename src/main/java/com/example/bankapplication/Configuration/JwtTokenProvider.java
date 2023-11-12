c979f0275c89e101c11cdaa9b08cd80be0a77530
Samuel <samuel.okafor@decagon.dev>
2023-11-12 16:05:55 +0100

    security

--- src/main/java/com/example/bankapplication/Configuration/JwtTokenProvider.java
+++ src/main/java/com/example/bankapplication/Configuration/JwtTokenProvider.java
@@ -1,9 +1,12 @@
 package com.example.bankapplication.Configuration;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
@@ -45,5 +48,18 @@ public String generateToken(Authentication authentication){
                 .getBody();
 
         return claims.getSubject();
+    }
+
+    public boolean validateToken(String token){
+    try{
+        Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parse(token);
+        return true;
+    } catch (ExpiredJwtException | MalformedJwtException | SignatureException | IllegalArgumentException e) {
+        throw new RuntimeException(e);
+    }
+
     }
 }
