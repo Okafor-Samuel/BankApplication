7ba262a6c2f6c418b37fbdff52250472cd3550a3
Samuel <samuel.okafor@decagon.dev>
2023-11-17 14:37:48 +0100

    security

--- src/main/java/com/example/bankapplication/Entity/User.java
+++ src/main/java/com/example/bankapplication/Entity/User.java
@@ -9,11 +9,13 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -40,6 +42,7 @@ public class User implements UserDetails {
     private String phoneNumber;
     private String alternativePhoneNumber;
     private String status;
+    private Role role;
     @CreationTimestamp
     private LocalDateTime createdAt;
     @UpdateTimestamp
@@ -47,31 +50,37 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of(new SimpleGrantedAuthority(role.name()));
+
+    }
+
+    @Override
+    public String getPassword() {
+        return password ;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return email;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
