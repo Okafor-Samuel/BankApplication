5fc71d7ec1d1b725fbcaa95e940dbae5591cec48
Samuel <samuel.okafor@decagon.dev>
2023-11-17 06:01:14 +0100

    security

--- src/main/java/com/example/bankapplication/Repository/UserRepository.java
+++ src/main/java/com/example/bankapplication/Repository/UserRepository.java
@@ -11,4 +11,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByEmail(String email);
     Boolean existsByAccountNumber(String accountNumber);
     Optional<User> findByAccountNumber(String accountNumber);
+    Optional<User> findByEmail(String email);
 }
